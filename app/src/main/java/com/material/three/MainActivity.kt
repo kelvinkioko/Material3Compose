9a20e467175067237525ab292040ef54a109b60d
Kelvin Kioko <kelvinmkioko@gmail.com>
2022-06-14 20:17:43 +0300

    [theme-v1] Implemented material 3 theme colors

--- app/src/main/java/com/material/three/MainActivity.kt
+++ app/src/main/java/com/material/three/MainActivity.kt
@@ -10,13 +10,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.material.three.ui.theme.Material3ComposeTheme
+import com.material.three.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Material3Compose)
         super.onCreate(savedInstanceState)
         setContent {
-            Material3ComposeTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -34,10 +35,10 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    Material3ComposeTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
